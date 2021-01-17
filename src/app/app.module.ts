be24268269a806082638810cd948485300c17320
Szym <szymmm@gmail.com>
2021-01-17 09:23:04 +0100

    add new components:
    - offert.component
    - how-it-works.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,9 @@ import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.com
 import { CameraComponent } from "./pages/camera/camera.component";
 import { EditBarComponent } from "./components/edit-bar/edit-bar.component";
 import { FormsModule } from "@angular/forms";
-import { FieldsComponent } from './pages/grzadka/fields/fields.component';
+import { FieldsComponent } from "./pages/grzadka/fields/fields.component";
+import { OffertComponent } from "./pages/home/offert/offert.component";
+import { HowItWorksComponent } from "./pages/home/how-it-works/how-it-works.component";
 
 @NgModule({
   declarations: [
@@ -22,6 +24,8 @@ import { FieldsComponent } from './pages/grzadka/fields/fields.component';
     CameraComponent,
     EditBarComponent,
     FieldsComponent,
+    OffertComponent,
+    HowItWorksComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
