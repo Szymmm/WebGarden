8329db529da208ede1dbc28d5c195425d8564d4d
Szym <szymmm@gmail.com>
2021-02-26 21:35:07 +0100

    add farms page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.com
 import { CameraComponent } from "./pages/camera/camera.component";
 import { ArticlesComponent } from "./pages/articles/articles.component";
 import { PricesComponent } from "./pages/prices/prices.component";
+import { FarmsComponent } from "./pages/farms/farms.component";
 
 const routes: Routes = [
   {
@@ -28,6 +29,10 @@ const routes: Routes = [
   {
     path: "prices",
     component: PricesComponent,
+  },
+  {
+    path: "farms",
+    component: FarmsComponent,
   },
   {
     path: "articles",
